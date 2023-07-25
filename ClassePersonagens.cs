eccc873d786b8a83fba61946386790e417d8a356
MiltomSantos <122657746+MiltomSantos@users.noreply.github.com>
2023-07-25 17:19:04 -0300

    Add files via upload

---
+++ ClassePersonagens.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula12Classes
+{
+    internal class ClassePersonagens
+    {
+        //variaeis (atributos)
+        public string nome;
+        public string raca;
+        public int poder;
+
+        //funcoes (metodos)
+        public void mostra()
+        {
+            Console.WriteLine($"Seu personagem é {raca} o nome é {nome} com {poder} de poder");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+using Aula12Classes;
+
+ClassePersonagens guerreiro = new ClassePersonagens();
+guerreiro.nome = "Gwyn-Lorde das Cinzas";
+guerreiro.raca = "Humano";
+guerreiro.poder = 300;
+
+guerreiro.mostra();
+
+ClassePersonagens mago = new ClassePersonagens();
+mago.nome = "Renalla-Rainha da Lua Cheia";
+mago.raca = "Elfo";
+mago.poder = 150;
+
+mago.mostra();
+
+ClassePersonagens bruxo = mago;
+bruxo.nome = "Ranni-The Witch";
+
+bruxo.mostra();
+
+
+ClassePersonagens anao = new ClassePersonagens();
+anao.nome = "Gael-Slave Knight";
+anao.raca = "Anão";
+anao.poder = 200;
+
+anao.mostra();
+
+
+
+
+
+Console.ReadKey();
